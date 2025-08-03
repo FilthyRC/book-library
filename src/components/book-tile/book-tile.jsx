97d9a53102bdb533ccce3fd192d9fb57d198f0d1
FilthyRC <ledzep.rc@gmail.com>
2025-08-04 00:48:07 +0100

    removed template started development

---
+++ src/components/book-tile/book-tile.jsx
@@ -0,0 +1,15 @@
+import "./book-tile.css";
+
+function BookTile() {
+  return (
+    <>
+      <div className="tile-container">
+        <p>img</p>
+        <h1>Title</h1>
+        <p>author</p>
+      </div>
+    </>
+  );
+}
+
+export default BookTile;
